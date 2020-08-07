fc1e19eb5e7269ccd4bb972b54ce4ed50b3a810b
SJJRAJU <johntheking071194@gmail.com>
2020-08-07 12:27:27 +0530

    Updates

---
+++ precache-manifest.ebb410c4d2e1e2dad958e2d8919b679a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e8346e36df5c61294ca485cd23eb8a",
+    "url": "/task-movie/index.html"
+  },
+  {
+    "revision": "88faf928e6e342a1d358",
+    "url": "/task-movie/static/css/main.0022ba46.chunk.css"
+  },
+  {
+    "revision": "e122d20bcc3d8189d7ce",
+    "url": "/task-movie/static/js/2.753eb1f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task-movie/static/js/2.753eb1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88faf928e6e342a1d358",
+    "url": "/task-movie/static/js/main.04cf756b.chunk.js"
+  },
+  {
+    "revision": "7d85b09bc0920ed5cd6e",
+    "url": "/task-movie/static/js/runtime-main.9f832575.js"
+  }
+]);
